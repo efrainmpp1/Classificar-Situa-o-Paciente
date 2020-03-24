a798f5232975c7f3bbdde13ad88b98ee0f3bf312
unknown <efrainmpp@hotmail.com>
2020-03-24 12:57:28 -0300

    teste2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask , jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,13 @@ cors = CORS(app, resource={r"/*":{"origins": "*"}})
 def index():
     return "<h1>Hello World!</h1>"
 
-@app.route("/deploy", methods=['GET'])    
-def deploy():
-    return "<h1>Testando deploy GitHub x Heroku </h1>"
+@app.route("/secundaria", methods=['GET'])    
+def secundaria():
+    return "<h1>Pagina Secundaria </h1>"
+
+@app.route("/testejson") 
+def testejson():
+	return jsonify({"ObjetoTemperatura" : "temperatura"})
 
 def main():
     port = int(os.environ.get("PORT", 5000))
