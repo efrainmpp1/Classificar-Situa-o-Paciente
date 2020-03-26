1b88125fb9f0bd753a6c0e6817fda4b82338a71f
unknown <efrainmpp@hotmail.com>
2020-03-26 17:31:05 -0300

    Testando requisição de dados

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import os
+from os.path import isfile
 from flask import Flask , jsonify , request
 from flask_cors import CORS
+from json import dump , load
+
+def lerjson():
+	with open('dados.json' , 'r' , encoding='utf8') as f:
+		return load(f)
 
 app = Flask(__name__)
 
@@ -8,29 +14,30 @@ cors = CORS(app, resource={r"/*":{"origins": "*"}})
 
 @app.route("/", methods=['GET'])
 def index():
-    return "<h1>Hello Efrain!</h1>"
-
-@app.route("/secundaria", methods=['GET'])    
-
-def secundaria():
-    return "<h1>Pagina Secundaria </h1>"
-
-@app.route("/testejson") 
-def testejson():
-	return jsonify({"ObjetoTemperatura" : 'temperatura'})
+    return "<h1>Teste APP PROCESSOS DE DADOS!</h1>"
 
 
 @app.route('/processojson' , methods=['POST'])
 def processocomjson():
-	dados = request.get_json()
+	'''dados = request.get_json()
 
 	nome = dados['nome']
 
 	localizacao = dados['localizacao']
 
+	temperatura = dados['temperatura'] '''
+
+	dados = lerjson()
+
+	localizacao = dados['localizacao']
+
 	temperatura = dados['temperatura']
 
-	return jsonify({'enviado' : 'ok' , 'nome' : nome , 'localizacao' : localizacao , 'temperatura' : temperatura})
+	diarreia = dados['diarreia']
+
+	espirro = dados['espirro']
+
+	return jsonify({'entregue' : 'ok' , 'localizacao' : localizacao , 'temperatura' : temperatura , 'diarreia' : diarreia , 'espirro' : espirro})
 
 def main():
     port = int(os.environ.get("PORT", 5000))
