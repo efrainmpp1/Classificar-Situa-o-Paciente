6850ad439c885df26c2a613b37a93c52119cfd33
unknown <efrainmpp@hotmail.com>
2020-04-03 17:48:24 -0300

    Correção

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
-from os.path import isfile
-from flask import Flask , jsonify , request
+from flask import Flask ,  request
 from flask_cors import CORS
-#from json import dump , load
+import json
 
-def lerjson():
-	with open('dados.json' , 'r' , encoding='utf8') as f:
-		return load(f)
+#Definindo variaveis previamente
+idoso = 0
+sangue = 0
+pulmao = 0
 
 app = Flask(__name__)
 
@@ -14,32 +14,58 @@ cors = CORS(app, resource={r"/*":{"origins": "*"}})
 
 @app.route("/", methods=['GET'])
 def index():
-    return "<h1>Teste APP PROCESSOS DE DADOS!</h1>"
+    return "<h1>Teste APP Classificador de Situações!</h1>"
 
-'''
-@app.route('/processojson' , methods=['POST'])
-def processocomjson():
+@app.route('/classificar' , methods=['POST' , 'GET'])
+def classificar():
 	dados = request.get_json()
-
-	nome = dados['nome']
-
-	localizacao = dados['localizacao']
-
-	temperatura = dados['temperatura'] 
-
-	dados = lerjson()
-
-	localizacao = dados['localizacao']
-
+	idade = dados['idade']
+	comorbidade = dados['comorbidade']
+	contatoSuspeito = dados['contatoSuspeito']
 	temperatura = dados['temperatura']
-
+	pulso = dados['pulso']
+	respiracao = dados['respiracao']
+	tosseSeca = dados['tosseSeca']
 	diarreia = dados['diarreia']
+	fApetite = dados['fApetite']
+	odores = dados['odores']
+	sabores = dados['sabores']
+	dorGarganta = dados['dorGarganta']
+	dorCorpo = dados['dorCorpo']
+	nariz = dados['nariz']
+	fadiga = dados['fadiga']
+	nausea = dados['nausea']
+	febre = 0
 
-	espirro = dados['espirro']
+	if idade > 50:
+		idoso = 1
 
-	return jsonify({'entregue' : 'ok' , 'localizacao' : localizacao , 'temperatura' : temperatura , 'diarreia' : diarreia , 'espirro' : espirro})
+	if temperatura > 37:
+		febre = 1
+	
+	if pulso < 60:
+		sangue = 1
 
-'''
+	if respiracao > 20:
+		pulmao = 1
+	
+	if (comorbidade == 0):
+		if (febre == 0) and ((tosseSeca == 0) or (nariz == 0) or (dorGarganta == 0) or (pulmao == 0)):
+			return { 'situacao' : 'Situaçao 1'}
+		else:
+			if idoso == 1 :
+				return { 'situacao' : 'Situaçao 4'}
+			else:
+				return { 'situacao' : 'Situaçao 2'}
+
+	else:
+		if (febre == 0) and ((tosseSeca == 0) or (nariz == 0) or (dorGarganta == 0) or (pulmao == 0)):
+			if idoso == 1:
+				return {'situacao' : 'Situação 5'}
+
+			else:
+				return {'situacao' : 'Situação 3'}
+
 
 def main():
     port = int(os.environ.get("PORT", 5000))
